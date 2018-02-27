7c7c9aacbf0e76aef76b6b2a853349672c7cdcca
Gorgelmug <31033463+Gorgelmug@users.noreply.github.com>
2018-02-27 11:45:15 +0100

    Add files via upload

---
+++ Counting Letters/Counting Letters/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Counting_Letters
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BrowseForTexts = new System.Windows.Forms.OpenFileDialog();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.txtInputLetter = new System.Windows.Forms.TextBox();
+            this.txtWelkeLetter = new System.Windows.Forms.TextBox();
+            this.btnZoek = new System.Windows.Forms.Button();
+            this.ltrsAantalLabel = new System.Windows.Forms.Label();
+            this.lettersAantalLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BrowseForTexts
+            // 
+            this.BrowseForTexts.FileName = "openFileDialog1";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(13, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(114, 32);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse your texts";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(134, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(432, 371);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // txtInputLetter
+            // 
+            this.txtInputLetter.Location = new System.Drawing.Point(13, 77);
+            this.txtInputLetter.MaxLength = 1;
+            this.txtInputLetter.Name = "txtInputLetter";
+            this.txtInputLetter.Size = new System.Drawing.Size(114, 20);
+            this.txtInputLetter.TabIndex = 2;
+            // 
+            // txtWelkeLetter
+            // 
+            this.txtWelkeLetter.Location = new System.Drawing.Point(13, 51);
+            this.txtWelkeLetter.Name = "txtWelkeLetter";
+            this.txtWelkeLetter.ReadOnly = true;
+            this.txtWelkeLetter.Size = new System.Drawing.Size(114, 20);
+            this.txtWelkeLetter.TabIndex = 3;
+            this.txtWelkeLetter.Text = "Welke letter zoek je?";
+            // 
+            // btnZoek
+            // 
+            this.btnZoek.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnZoek.Location = new System.Drawing.Point(13, 104);
+            this.btnZoek.Name = "btnZoek";
+            this.btnZoek.Size = new System.Drawing.Size(114, 28);
+            this.btnZoek.TabIndex = 4;
+            this.btnZoek.Text = "Zoek";
+            this.btnZoek.UseVisualStyleBackColor = true;
+            this.btnZoek.Click += new System.EventHandler(this.btnZoek_Click);
+            // 
+            // ltrsAantalLabel
+            // 
+            this.ltrsAantalLabel.AutoSize = true;
+            this.ltrsAantalLabel.Location = new System.Drawing.Point(13, 139);
+            this.ltrsAantalLabel.Name = "ltrsAantalLabel";
+            this.ltrsAantalLabel.Size = new System.Drawing.Size(97, 13);
+            this.ltrsAantalLabel.TabIndex = 5;
+            this.ltrsAantalLabel.Text = "Het aantal letters is";
+            // 
+            // lettersAantalLabel
+            // 
+            this.lettersAantalLabel.AutoSize = true;
+            this.lettersAantalLabel.Location = new System.Drawing.Point(13, 163);
+            this.lettersAantalLabel.Name = "lettersAantalLabel";
+            this.lettersAantalLabel.Size = new System.Drawing.Size(0, 13);
+            this.lettersAantalLabel.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(16, 262);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(100, 109);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "Om een text toe te voegen klik op Browse. Selecteer de text door dubbel te kikken" +
+    " en voer deletter in die je wilt weergeven. Klik dan op Zoek";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 383);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lettersAantalLabel);
+            this.Controls.Add(this.ltrsAantalLabel);
+            this.Controls.Add(this.btnZoek);
+            this.Controls.Add(this.txtWelkeLetter);
+            this.Controls.Add(this.txtInputLetter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog BrowseForTexts;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox txtInputLetter;
+        private System.Windows.Forms.TextBox txtWelkeLetter;
+        private System.Windows.Forms.Button btnZoek;
+        private System.Windows.Forms.Label ltrsAantalLabel;
+        private System.Windows.Forms.Label lettersAantalLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Counting Letters/Counting Letters/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace Counting_Letters
+{
+    public partial class Form1 : Form
+    {
+        //a string that holds the text from the file
+        private string text;
+
+        //the list that holds the character
+        private List<char> countList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            countList = new List<char>();
+        }
+
+        //Button for browsing the pc for a text
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //this makes the browsing happening an sets the text to the TextBox
+            DialogResult result = BrowseForTexts.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                int size = -1;
+
+                string file = BrowseForTexts.FileName;
+                try
+                {
+                    text = File.ReadAllText(file);
+                    size = text.Length;
+                    richTextBox1.Text = text;
+                }
+                catch (IOException)
+                {
+                }
+            }
+        }
+
+        //Button for searching the text for a specific character and outputs the number it is in the text
+        private void btnZoek_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //clearing the variables
+                lettersAantalLabel.Text = String.Empty;
+                countList.Clear();
+
+                //setting the character needed for the counting part
+                char input = Convert.ToChar(txtInputLetter.Text);
+               
+
+                //sets the number of counts for the loop so input can get into the list
+                string inCount = "";
+                inCount.ToLower();
+                inCount = Convert.ToString(input);
+                inCount.Count();
+                
+                //this part gets the character and loops it and at the same time
+                // it adds the character to the list
+                //ToLower make all the chars to lowercase in order to count them all
+                foreach (char count in text.ToLower())
+                {
+                    for (int i = 0; i < inCount.Count(); i++)
+                    {
+                        if (count == input)
+                        {
+                            countList.Add(input);
+                        }
+                    }
+
+                    //output to the screen (number of characters)
+                    lettersAantalLabel.Text = Convert.ToString(countList.Count);
+                }
+             }
+            catch
+            {
+                MessageBox.Show("Geen of ongeldige invoer!" + Environment.NewLine + "Is er wel tekst toegevoegd?");
+            }
+        }
+    }
+}
+
+
+
